5d2a67c826b448ed89bef097cdd3455a000d9845
Val <88689766+valli450@users.noreply.github.com>
2022-04-05 11:48:29 -0400

    Update main-destination.js

--- JS/main-destination.js
+++ JS/main-destination.js
@@ -4,10 +4,14 @@ const menuBlock = document.querySelector(".header__menu");
 
 function openMenu() {
     menuBlock.classList.add("showMenu");
+    document.querySelector(".header__humburger").style.display = 'none';
+
 }
 
 function closeMenu() {
     menuBlock.classList.remove("showMenu");
+    document.querySelector(".header__humburger").style.display = 'block';
+
 }
 
 /* Planet switch script */
