502840ab748105c3181d9d79136b56a0b2b25d03
Val <88689766+valli450@users.noreply.github.com>
2022-04-05 11:48:40 -0400

    Update main-home.js

--- JS/main-home.js
+++ JS/main-home.js
@@ -4,8 +4,12 @@ const menuBlock = document.querySelector(".header__menu");
 
 function openMenu() {
     menuBlock.classList.add("showMenu");
+    document.querySelector(".header__humburger").style.display = 'none';
+
 }
 
 function closeMenu() {
     menuBlock.classList.remove("showMenu");
+    document.querySelector(".header__humburger").style.display = 'block';
+
 }
