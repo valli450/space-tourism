d5fba343bcfe3b0663f15ba648f3b3be24fc096e
Val <88689766+valli450@users.noreply.github.com>
2022-04-05 11:48:50 -0400

    Update technology.js

--- JS/technology.js
+++ JS/technology.js
@@ -3,10 +3,14 @@ const menuBlock = document.querySelector(".header__menu");
 
 function openMenu() {
     menuBlock.classList.add("showMenu");
+    document.querySelector(".header__humburger").style.display = 'none';
+
 }
 
 function closeMenu() {
     menuBlock.classList.remove("showMenu");
+    document.querySelector(".header__humburger").style.display = 'block';
+
 }
 
 /* Technology script */
